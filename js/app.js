7eb8b90c12df84b313ce22b8f94d680fe8e1e60d
TheGrimStever <senior.baca@gmail.com>
2016-04-11 21:43:22 -0600

    Incomplete, on last bullet of Step 4.

--- js/app.js
+++ js/app.js
@@ -5,5 +5,25 @@ app.config(function ($stateProvider, $urlRouterProvider, $httpProvider) {
     $httpProvider.interceptors.push('httpRequestInterceptor');
 
     // routing configuration code
+    $urlRouterProvider
+      .otherwise('/');
 
+    $stateProvider
+
+      .state('home', {
+        url: '/',
+        controller: 'homeCtrl',
+        templateUrl: 'js/home/homeTmpl.html'
+      })
+
+      .state('teams', {
+        url: '/teams/:team',
+        controller: 'teamCtrl',
+        resolve: {
+          teamData: function (teamService, $stateParams) {
+            return teamService.getTeamData($stateParams.team);
+          }
+        },
+        templateUrl: 'js/teams/teamTmpl.html'
+      })
 });
